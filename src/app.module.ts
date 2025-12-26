6f136f50c5f144ba59d813c25db09c6c49bb65b9
dounia-elg <dounia.elg20@gmail.com>
2025-12-26 09:16:31 +0100

    feat: add TypeORM and PostgreSQL support, integrate ConfigModule and MongodbModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config' ; 
+import { MongodbModule } from './database/mongodb/mongodb.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, MongodbModule],
   controllers: [AppController],
   providers: [AppService],
 })
