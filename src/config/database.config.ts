287ada87640ace2d8885ccf1cf10f91d575a2b73
dounia-elg <dounia.elg20@gmail.com>
2025-12-28 22:37:58 +0100

    feat: GED-10 configure PostgreSQL database connection with TypeORM

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,13 @@
 import { registerAs } from '@nestjs/config'
 
 export const mongoConfig = registerAs('mongo', () => ({
-    uri: process.env.MONGO_URI || 'mongodb://localhost:27017/gedpro',   
+    uri: process.env.MONGO_URI,
+}));
+
+export const postgresConfig = registerAs('postgres', () => ({
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
 }));
