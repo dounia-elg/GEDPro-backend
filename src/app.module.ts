48f57f7f8f79d9aa96034b1fd2c040052209c148
dounia-elg <dounia.elg20@gmail.com>
2025-12-28 22:40:13 +0100

    feat: GED-10 configure global database settings with ConfigModule for PostgreSQL and MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config' ; 
 import { MongodbModule } from './database/mongodb/mongodb.module';
+import { PostgresqlModule } from './database/postgresql/postgresql.module';
+import { mongoConfig, postgresConfig } from './config/database.config';
 
 @Module({
-  imports: [ConfigModule, MongodbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [mongoConfig, postgresConfig],
+      envFilePath: '.env',
+    }),
+    MongodbModule,
+    PostgresqlModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
