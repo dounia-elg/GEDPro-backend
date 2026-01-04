de83581049d9a014ca24037ec2a597d58842a7bf
dounia-elg <dounia.elg20@gmail.com>
2026-01-04 23:36:20 +0100

    feat:GED-48 add Form entity with fields, timestamps, and relationships

---
+++ src/forms/entities/form.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { FormField } from './form-field.entity';
+
+@Entity('forms')
+export class Form {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column()
+  createdBy: string; 
+
+  @OneToMany(() => FormField, (field) => field.form, { cascade: true, eager: true })
+  fields: FormField[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
