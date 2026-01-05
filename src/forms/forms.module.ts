b1ce65a2676a3a04d36877a515ffdc170177cb33
dounia-elg <dounia.elg20@gmail.com>
2026-01-05 11:21:40 +0100

    feat: GED-48 update FormsModule to include TypeOrmModule for Form and FormField entities

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { FormsController } from './forms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormsService } from './forms.service';
+import { FormsController } from './forms.controller';
+import { Form } from './entities/form.entity';
+import { FormField } from './entities/form-field.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Form, FormField])],
   controllers: [FormsController],
-  providers: [FormsService]
+  providers: [FormsService],
 })
 export class FormsModule {}
