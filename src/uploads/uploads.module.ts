81e6b744bc15a1303fb237d030d8e8cca761708f
dounia-elg <dounia.elg20@gmail.com>
2026-01-05 22:22:06 +0100

    feat: GED-64 add OcrModule to UploadsModule imports for OCR functionality

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadsController } from './uploads.controller';
 import { MinioModule } from '../minio/minio.module';
+import { OcrModule } from '../ocr/ocr.module';
 
 @Module({
-  imports: [MinioModule],
+  imports: [MinioModule, OcrModule],
   controllers: [UploadsController],
 })
 export class UploadsModule {}
